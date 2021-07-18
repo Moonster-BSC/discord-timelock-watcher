3297554bfc60c640f0071d6df5e010c6549fc028
Moonster <Moonster.bsc@gmail.com>
2021-07-18 17:18:35 -0400

    ListAccountTransactionsResponse

---
+++ src/types/etherscanApi/ListAccountTransactionsResponse.ts
@@ -0,0 +1,26 @@
+interface Result {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  transactionIndex: string;
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  gasPrice: string;
+  isError: string;
+  txreceipt_status: string;
+  input: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  gasUsed: string;
+  confirmations: string;
+}
+
+export interface ListAccountTransactionsResponse {
+  status: string;
+  message: string;
+  result: Result[];
+}
