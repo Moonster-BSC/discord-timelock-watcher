034d22b0a5be73519855b2ad7a2ab3ffeff3423e
Moonster <Moonster.bsc@gmail.com>
2021-07-19 01:19:31 -0400

    removeTimelockHandler

---
+++ src/eventsHandlers/messageEventHandler/commandHandlers/removeTimelockHandler.ts
@@ -0,0 +1,17 @@
+import { ChainId } from "blockchain-addressbook";
+import { MessageArgumentReader } from "discord-command-parser";
+import { removeTimelock } from "../../../state/state";
+import { SupportedChainId } from "../../../types";
+
+export const removeTimelockHandler = (reader: MessageArgumentReader): void => {
+  // format is !add <name> <address> <chainid> Chain id is nickname, not number
+  const nickname = reader.getString();
+  const chainId = reader.getString();
+
+  // should be chainId in a supported chain id map, but this doesn't exist yet.
+  if (nickname === null || chainId === null || chainId in ChainId === false) {
+    return;
+  }
+
+  removeTimelock(chainId as unknown as SupportedChainId, nickname);
+};
