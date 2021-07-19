bee73cfc305ce641d64b881bc2174ef3cf658484
Moonster <Moonster.bsc@gmail.com>
2021-07-19 02:29:37 -0400

    import dotenv from "dotenv"; dotenv.config();

--- src/bot.ts
+++ src/bot.ts
@@ -4,6 +4,9 @@ const bot = new Discord.Client();
 
 export type BotEvents = Extract<keyof Discord.ClientEvents, "message">;
 
+import dotenv from "dotenv";
+dotenv.config();
+
 // so we easily know what events are implemented
 const botEventNames: Record<BotEvents, BotEvents> = {
   message: "message",
