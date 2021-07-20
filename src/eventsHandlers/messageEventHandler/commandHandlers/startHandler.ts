1f2e1b4c28d0d391398ab1944c0e76b9287e084d
Moonster <Moonster.bsc@gmail.com>
2021-07-20 01:00:19 -0400

    start command protection

--- src/eventsHandlers/messageEventHandler/commandHandlers/startHandler.ts
+++ src/eventsHandlers/messageEventHandler/commandHandlers/startHandler.ts
@@ -1,7 +1,7 @@
 import Discord from "discord.js";
 
 import { MessageArgumentReader } from "discord-command-parser";
-import { setIsRunning } from "../../../state/state";
+import { getIsRunning, setIsRunning } from "../../../state/state";
 import { driver } from "../../../driver/driver";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,6 +9,12 @@ export const startHandler = (
   _reader: MessageArgumentReader,
   channel: Discord.TextChannel
 ): void => {
+  if (getIsRunning() === true) {
+    const message = `Bot is already running`;
+    channel.send(message);
+    return;
+  }
+
   setIsRunning(true);
 
   driver(channel);
