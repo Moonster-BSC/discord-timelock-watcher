0de8aa962f50f1bedc4d8f3d964a1cb959f533a0
Moonster <Moonster.bsc@gmail.com>
2021-07-21 12:33:22 -0400

    messagechannel id

--- src/eventsHandlers/messageEventHandler/messageEventHandler.ts
+++ src/eventsHandlers/messageEventHandler/messageEventHandler.ts
@@ -19,9 +19,12 @@ const commandPrefix = "!";
 export const messageEventHandler = (message: Discord.Message): void => {
   const devRoleId = process.env["dev_role_id"];
   const beefyOgRoleId = process.env["beefyog_role_id"]; // TODO move this to config file and make it array of roles
+
+  const isTimelockChannel = "867425212554543104";
   if (
     devRoleId !== undefined &&
     beefyOgRoleId != undefined &&
+    message.channel.id === isTimelockChannel &&
     message.member?.roles.cache.has(devRoleId) === false
   ) {
     // For some reason this gets sent like 10 times on my server. Not sure why this method is called so many times, maybe theres built in retry and the return statement is incorrect
